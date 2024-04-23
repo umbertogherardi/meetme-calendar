f25169af8212fae7af7702afb8d0fb098ccb4322
Umberto Gherardi <umberto.gherardi@gmail.com>
2024-04-23 15:34:29 -0600

    Add mongo connectivity

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,12 @@
 import express from "express";
 import cors from "cors";
+import { MongoClient } from "mongodb";
+
+async function connect() {
+  const client = new MongoClient('mongodb://localhost:27017');
+  const connection = await client.connect();
+  return connection.db("meetme-db");
+}
 
 const port = 3001;
 
@@ -7,6 +14,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const database = await connect();
+app.set("db", database);
 
 app.listen(port, () => {
   console.info(`Server is running at http://localhost:${port}`)
