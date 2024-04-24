ccaec511508d4d6981fc85de9c7cab536a82ef7a
Umberto Gherardi <umberto.gherardi@gmail.com>
2024-04-23 23:07:46 -0600

    Add frontend calendar view dropdown

--- frontend/meetme-app/src/components/Calendar.js
+++ frontend/meetme-app/src/components/Calendar.js
@@ -1,6 +1,21 @@
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import 'bootstrap/dist/js/bootstrap.bundle';
+
 function Calendar() {
+    const [view, setView] = useState("Week");
+
     return (
-        <p>This is your calendar.</p>
+    <div className="dropdown">
+        <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            {view}
+        </button>
+        <ul className="dropdown-menu">
+            <li><Link to="" className="dropdown-item" onClick={() => setView("Month")}>Month</Link></li>
+            <li><Link to="" className="dropdown-item" onClick={() => setView("Week")}>Week</Link></li>
+            <li><Link to="" className="dropdown-item" onClick={() => setView("Day")}>Day</Link></li>
+        </ul>
+    </div>
     );
 }
 
