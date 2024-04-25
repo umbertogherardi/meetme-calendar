08139d8c5f7336c05b003106c04b51d39c15e079
Umberto Gherardi <umberto.gherardi@gmail.com>
2024-04-25 12:16:35 -0600

    Update home text

--- frontend/meetme-app/src/components/Home.js
+++ frontend/meetme-app/src/components/Home.js
@@ -6,9 +6,9 @@ function Home() {
         <div style={{margin: '3rem'}}>
             <h3>Welcome to MeetMe</h3>
             <div style={{marginTop: '2rem', fontSize: 'large'}}>
-                <p>Welcome to MeetMe, a platform for all your calendar-related needs!</p>
-                <p>MeetMe gives you the traditional benefits of a calendar webapp, while also allowing you to view the calendars of people in your contacts list.</p>
-                <p>If you're a returing user, please <Link to={`${FRONTEND_URL}/login`}>login</Link>. New users should <Link to={`${FRONTEND_URL}/sign-up`}>sign up</Link> for a new account.</p>
+                <p>Welcome to MeetMe, a platform for all your scheduling needs!</p>
+                <p>MeetMe is similar to a traditional calendar application with the added capability of viewing contacts' calendars.</p>
+                <p>If you're a returning user, please <Link to={`${FRONTEND_URL}/login`}>login</Link>. New users should <Link to={`${FRONTEND_URL}/sign-up`}>sign up</Link> for a new account.</p>
             </div>
         </div>
     );
