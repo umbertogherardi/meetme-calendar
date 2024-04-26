6becd29888a02371b6380257b53914e4f118cd06
Umberto Gherardi <umberto.gherardi@gmail.com>
2024-04-25 18:51:52 -0600

    Add month view modal

--- frontend/meetme-app/src/components/MonthView.js
+++ frontend/meetme-app/src/components/MonthView.js
@@ -13,11 +13,29 @@ function MonthView() {
     const [daysInMonth, setDaysInMonth] = useState(moment().daysInMonth());
     const [firstDayOfMonth, setFirstDayOfMonth] = useState(moment().startOf('month').day());
 
+    const [eventName, setEventName] = useState("");
+    const [startTime, setStartTime] = useState("");
+    const [endTime, setEndTime] = useState("");
+    const [startAM, setStartAM] = useState(true);
+    const [endAM, setEndAM] = useState(true);
+
     const VIEW = "Month";
 
     const CURR_YEAR = moment().year();
     const CURR_MONTH = moment().month();
     const CURR_DAY = moment().date();
+
+    const [eventDay, setEventDay] = useState(-1);
+
+    function addEvent(event) {
+        event.preventDefault();
+        console.log(`Name: ${eventName}, Start Time: ${startTime}${startAM ? "AM" : "PM"}, End Time: ${endTime}${endAM ? "AM" : "PM"}`);
+        setEventName("");
+        setStartTime("");
+        setEndTime("");
+        setStartAM(true);
+        setEndAM(true);
+    }
 
     function decrementView() {
         if (month === 0) {
@@ -91,10 +109,6 @@ function MonthView() {
         }
     }
 
-    function addEvent(e){
-        console.log(e.target);
-    }
-
     let rowStartIdxs = setStartIdxs();
     let dayNumberings = setDayNumberings();
 
@@ -166,20 +180,83 @@ function MonthView() {
                 <div className="row" key={index}>
                     {dayNumberings.slice(index, index + 7).map((dayNumber) => (
                         ((year === CURR_YEAR) && (month === CURR_MONTH) && (dayNumber === CURR_DAY)) ?
-                        <div className="col border" key={dayNumber} id={dayNumber} style={{height: "14vh"}} onClick={addEvent}>
-                            <div style={{color: "white", backgroundColor: "#2596BE", borderRadius: "16px", display: "flex", justifyContent: "center", width: "2rem", margin: "auto", padding: "4px"}}>
+                        <div className="col border" key={dayNumber} id={dayNumber} style={{height: "14vh"}} data-bs-toggle="modal" data-bs-target="#eventModal" onClick={event => setEventDay(event.target.id)}>
+                            <div style={{color: "white", backgroundColor: "#2596BE", borderRadius: "16px", display: "flex", justifyContent: "center", width: "2rem", margin: "auto", padding: "4px", pointerEvents: "none"}}>
                                 {dayNumber}
                             </div>
                         </div>
                         :
-                        <div className="col border" key={dayNumber} id={dayNumber} style={{height: "14vh"}} onClick={addEvent}>
+                        (dayNumber > 0) ?
+                        <div className="col border" key={dayNumber} id={dayNumber} style={{height: "14vh"}} data-bs-toggle="modal" data-bs-target="#eventModal" onClick={event => setEventDay(event.target.id)}>
+                            <div style={{padding: "4px", pointerEvents: "none"}}> 
+                                {dayNumber}
+                            </div>
+                        </div>
+                        :
+                        <div className="col border" key={dayNumber} id={dayNumber} style={{height: "14vh"}}>
                             <div style={{padding: "4px"}}> 
-                                {dayNumber > 0 ? dayNumber : ""}
+                                {""}
                             </div>
                         </div>
                     ))}
                 </div>
             ))}
+        </div>
+        <div className="modal fade" id="eventModal" tabIndex="-1" aria-labelledby="eventModalLabel" aria-hidden="true">
+            <div className="modal-dialog">
+                <div className="modal-content">
+                <div className="modal-header">
+                    <h1 className="modal-title fs-5" id="eventModalLabel">{`${MONTH_ARR[month]} ${eventDay}, ${year} Event`}</h1>
+                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div className="modal-body">
+                    <form style={{margin: 'auto'}} onSubmit={addEvent}>
+                        <div className="mb-3">
+                            <label htmlFor="eventNameInput" className="form-label">Event Name</label>
+                            <input 
+                                className="form-control" id="eventNameInput" onChange={event => setEventName(event.target.value)}
+                                value={eventName}>
+                            </input>
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="startTimeInput" className="form-label">Start Time</label>
+                            <div style={{display: "flex", gap: "10px"}}>
+                                <input 
+                                    className="form-control" id="startTimeInput" onChange={event => setStartTime(event.target.value)}
+                                    value={startTime}>    
+                                </input>
+                                <div className="btn-group" role="group" aria-label="Start time AM/PM toggle radio buton group">
+                                    <input type="radio" className="btn-check" name="btnradio-start" id="btnradio-am-start" autoComplete="off" defaultChecked onChange={() => setStartAM(true)}></input>
+                                    <label className="btn btn-outline-secondary" htmlFor="btnradio-am-start">AM</label>
+                                    <input type="radio" className="btn-check" name="btnradio-start" id="btnradio-pm-start" autoComplete="off" onChange={() => setStartAM(false)}></input>
+                                    <label className="btn btn-outline-secondary" htmlFor="btnradio-pm-start">PM</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="endTimeInput" className="form-label">End Time</label>
+                            <div style={{display: "flex", gap: "10px"}}>
+                                <input 
+                                    className="form-control" id="endTimeInput" onChange={event => setEndTime(event.target.value)}
+                                    value={endTime}>
+                                </input>
+                                <div className="btn-group" role="group" aria-label="End time AM/PM toggle radio buton group">
+                                    <input type="radio" className="btn-check" name="btnradio-end" id="btnradio-am-end" autoComplete="off" defaultChecked onChange={() => setEndAM(true)}></input>
+                                    <label className="btn btn-outline-secondary" htmlFor="btnradio-am-end">AM</label>
+                                    <input type="radio" className="btn-check" name="btnradio-end" id="btnradio-pm-end" autoComplete="off" onChange={() => setEndAM(false)}></input>
+                                    <label className="btn btn-outline-secondary" htmlFor="btnradio-pm-end">PM</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div style={{display: "flex", justifyContent: "left", gap: "10px"}}>
+                            <button type="button" className="btn btn-light" data-bs-dismiss="modal">Close</button>
+                            <button type="button submit" className="btn btn-secondary" data-bs-dismiss="modal">Add Event</button>
+                        </div>  
+                    </form>
+                    <div></div>
+                </div>
+                </div>
+            </div>
         </div>
     </>
     );
